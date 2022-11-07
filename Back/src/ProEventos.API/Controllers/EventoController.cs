2a81b432166a59c3549c7935a79614f6dfec83ac
Leandro Cesar <leandro.cesar@globalsys.com.br>
2022-11-07 17:46:39 -0300

    controller evento

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento() {
+                EventoId = 1,
+                Local = "São Paulo",
+                DataEvento = "11/02/1991",
+                Tema = "Azul",
+                QtdPessoas = 250,
+                Lote = "4754b",
+                ImagemURL = "N/A"
+            },
+            new Evento() {
+                EventoId = 2,
+                Local = "Espirito Santo",
+                DataEvento = "10/10/1996",
+                Tema = "Vermelho",
+                QtdPessoas = 120,
+                Lote = "2222b",
+                ImagemURL = "N/A"
+            },
+
+        };
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == 2);
+        }
+
+    }
+}
