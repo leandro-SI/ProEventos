116d4609a83a567bea05beb17523c817f17f9026
Leandro Cesar <leandro.cesar@globalsys.com.br>
2022-11-08 15:14:58 -0300

    component eventos e palestrantes

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
