16e0293f81fbb128ac4e0637ff1cb06a760b9f05
Leandro <leandro.almeida@uvvnet.com.br>
2022-11-17 23:21:04 -0300

    resolvendo loop infinito com Newtonsoft

--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -34,7 +34,11 @@ namespace ProEventos.API
         {
 
             services.AddDbContext<ProEventosContext>(context => context.UseSqlite(Configuration.GetConnectionString("ProEventosConnection")));
-            services.AddControllers();
+
+            services.AddControllers()
+                .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling =
+                   Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
 
             services.AddScoped<IEventoService, EventoService>();
             services.AddScoped<IEventoPersist, EventoPersist>();
