2dbf2377c90b8d328315157bd87f973a758b5d44
Leandro Cesar <leandro.cesar@globalsys.com.br>
2022-12-12 14:53:39 -0300

    add angularCompilerOptions

--- Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
-import { Evento } from 'src/app/models/Evento';
+import { Evento } from '@app/models/Evento';
 import { EventoService } from 'src/app/services/evento.service';
 
 @Component({
