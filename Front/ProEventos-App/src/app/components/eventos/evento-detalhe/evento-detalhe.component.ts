00ecd743764ce4c9edc6b4597bbb81e124f19a53
Leandro Cesar <leandro.cesar@globalsys.com.br>
2022-12-13 10:51:20 -0300

    validação form detalhe evento

--- Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -10,22 +10,30 @@ export class EventoDetalheComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.validation();
   }
 
   public validation(): void {
-    this.form = new FormGroup({
-      tema: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
-      local: new FormControl('', Validators.required),
-      dataEvento: new FormControl('', Validators.required),
-      qtdPessoas: new FormControl('', [Validators.required, Validators.max(120000)]),
-      telefone: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      imagemURL: new FormControl('', Validators.required)
+    this.form = this.formBuilder.group({
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      imagemURL: ['', Validators.required]
     });
+  }
+
+  reseteForm(): void {
+    this.form.reset();
   }
 
 }
