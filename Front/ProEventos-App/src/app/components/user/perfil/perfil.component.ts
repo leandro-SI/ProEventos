3300ea8bd9816e9e2ff2fc277983bb949f114d74
Leandro Cesar <leandro.cesar@globalsys.com.br>
2022-12-14 11:51:21 -0300

    Confirmar senha - Must Match e form perfil

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,3 +1,5 @@
+import { ValidatorField } from './../../../helpers/ValidatorField';
+import { FormGroup, FormBuilder, Validators, AbstractControlOptions } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,9 +11,36 @@ export class PerfilComponent implements OnInit {
 
   public titulo = 'Perfil'
 
-  constructor() { }
+  form: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.validation();
   }
+
+  public validation(): any {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMach('senha', 'confirmaSenha')
+    };
+
+    this.form = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', Validators.required],
+      telefone: ['', Validators.required],
+      funcao: ['', Validators.required],
+      descricao: ['', Validators.required],
+      senha: ['', Validators.required],
+      confirmaSenha: ['', Validators.required]
+    }, formOptions)
+  }
+
 
 }
--- Front/ProEventos-App/src/app/components/user/registrar/registrar.component.ts
+++ Front/ProEventos-App/src/app/components/user/registrar/registrar.component.ts
@@ -1,3 +1,5 @@
+import { ValidatorField } from './../../../helpers/ValidatorField';
+import { AbstractControl, AbstractControlOptions } from '@angular/forms';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
@@ -21,6 +23,12 @@ export class RegistrarComponent implements OnInit {
   }
 
   public validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMach('senha', 'confirmaSenha')
+    };
+
+
     this.form = this.formBuilder.group({
       primeiroNome: ['', Validators.required],
       ultimoNome: ['', Validators.required],
@@ -28,7 +36,7 @@ export class RegistrarComponent implements OnInit {
       userName: ['', Validators.required],
       senha: ['', Validators.required],
       confirmaSenha: ['', Validators.required]
-    });
+    }, formOptions);
   }
 
   reseteForm(): void {
